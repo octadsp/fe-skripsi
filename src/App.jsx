869b072f5a6f53873e5338d13816646024cdbf62
toleee <octaadsp.camp@gmail.com>
2023-09-01 22:54:05 +0700

    Development

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">Testing</h1>
+        <h1 className="text-3xl font-bold underline">Testing Development</h1>
       </div>
     </>
   );
