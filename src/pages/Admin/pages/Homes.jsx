9e4115793f8e9f15e1ebe30d5ae9bf14bace37c6
toleee <octaadsp.camp@gmail.com>
2023-12-16 01:19:35 +0700

    [Update] Last update 15-12-23

--- src/pages/Admin/pages/Homes.jsx
+++ src/pages/Admin/pages/Homes.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import InputForm from "../../../components/Elements/InputForm";
+import Header from "../../../assets/kop.png";
+
+import { useMutation, useQuery } from "react-query";
+import { API } from "../../../config/api";
+
+import ErrorAlert from "../../../components/Elements/ErrorAlert";
+import SuccessAlert from "../../../components/Elements/SuccessAlert";
 
 function Homes() {
-  return <div>Homes</div>;
+  return (
+    <>
+      <div className="flex justify-center mb-20 mt-5">
+        <img src={Header} />
+      </div>
+      <div className="flex m-16 gap-5 text-navBg justify-center">
+        <p>Selamat datang di halaman admin aplikasi</p>
+      </div>
+    </>
+  );
 }
 
 export default Homes;
